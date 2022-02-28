2a132fb9d3ce49b1bdabefc8ea59c1523058d2a3
Estefano Delgado <fanodev310@gmail.com>
2022-02-28 18:22:36 -0300

    fallo en agregar dinamismo con DOM en el instafeed

--- assets/js/main.js
+++ assets/js/main.js
@@ -238,5 +238,47 @@
     })
   });
 
-})()
+})();
 
+console.log(document.getElementById("instafeed"));
+let insta =  document.getElementById("instafeed");
+  var feed = new Instafeed({
+    accessToken: 'IGQVJVOTYtUVVlLTRCS0dxeDZAybTVUZAVduYW9kbzl1ZAWtvNllMdmRibGE1NDU5VTVDOHd5R3UxZAldBbVBtQ2tDdWE5ZAmN2Tnl1V0VmUDAxNlpZARTR6dElBbWxRU2N2ZAEZAfaU1VbDNOMlQtaUdxUDMtVQZDZD',
+    get: 'user',
+    userId: '44782484749', 
+    limit: 12,
+    template: '<div class="img-instafeed center col-lg-12 col-md-6 col-xs-6 thumb"><a href="{{link}}"><img class="lazyOwl" title="{{caption}}" src="{{image}}" data-src="{{caption}}"  /></a> </div>',
+    after: function(){
+      $('.owl-carousel').owlCarousel({
+        loop:true,
+        // lazyLoad:true,
+        autoplay:true,
+        autoplayTimeout:4000,
+        autoplayHoverPause:true,
+        // itemsDesktop: [1199, 3],
+        // itemsDesktopSmall: [979, 3],
+        margin:10,
+        dots: true,
+        //	nav:true,
+        // navText:['<','>'],
+        responsiveClass:true,
+        responsive:{
+          0:{
+            items:2
+            
+          },
+          600:{
+            items:3,
+            loop:true
+            
+          },
+          1000:{
+            items:3,							
+            loop:true
+          }
+        }
+      });
+    }
+  });
+  feed.run();
+insta.run();
